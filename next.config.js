b5b2e0af95e872413513c29e0995de7136765b61
Andrey Strogov <asthmaisback@gmail.com>
2023-04-04 18:48:24 +0400

    fix: update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withMarkdoc = require('@markdoc/next.js');
 
+const repo = 'nextjs-gh-pages'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 module.exports =
   withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
-    output: 'export',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
   });
